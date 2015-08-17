facdd0932c2b4a6455e9707c4065840adce98502
CosmoAbdon <cosmo.webp2@gmail.com>
2015-08-16 23:06:15 -0300

    #12.11
    Change layout MainForm

--- src/Forms/MainForm.java
+++ src/Forms/MainForm.java
@@ -46,7 +46,6 @@ public final class MainForm extends javax.swing.JFrame {
         Integer year = datm.getYear();
         preencher_jtable(day, month, year);
         
-
     }
     
 
@@ -66,10 +65,11 @@ public final class MainForm extends javax.swing.JFrame {
         jc_day = new com.toedter.calendar.JDayChooser();
         jc_month = new com.toedter.calendar.JMonthChooser();
         jc_year = new com.toedter.calendar.JYearChooser();
-        jButton1 = new javax.swing.JButton();
+        jb_consul = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
         jtf_search = new javax.swing.JTextField();
+        jb_ok = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -126,33 +126,11 @@ public final class MainForm extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(53, 57, 65));
 
-        jc_day.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent evt) {
-                jc_dayPropertyChange(evt);
-            }
-        });
-
         jc_month.setDayChooser(jc_day);
-        jc_month.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent evt) {
-                jc_monthPropertyChange(evt);
-            }
-        });
 
         jc_year.setDayChooser(jc_day);
-        jc_year.setFont(new java.awt.Font("Arial Black", 0, 12)); // NOI18N
-        jc_year.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent evt) {
-                jc_yearPropertyChange(evt);
-            }
-        });
 
-        jButton1.setText("Realizar Consulta !");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        jb_consul.setText("Realizar Consulta !");
 
         org.jdesktop.layout.GroupLayout jPanel2Layout = new org.jdesktop.layout.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -161,12 +139,12 @@ public final class MainForm extends javax.swing.JFrame {
             .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                    .add(jc_day, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(jc_day, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE)
                     .add(jPanel2Layout.createSequentialGroup()
                         .add(jc_month, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                        .add(92, 92, 92)
-                        .add(jButton1)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 202, Short.MAX_VALUE)
+                        .add(129, 129, 129)
+                        .add(jb_consul)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .add(jc_year, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 75, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
@@ -176,7 +154,7 @@ public final class MainForm extends javax.swing.JFrame {
                 .add(11, 11, 11)
                 .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(jc_month, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jButton1)
+                    .add(jb_consul)
                     .add(jc_year, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .add(18, 18, 18)
                 .add(jc_day, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
@@ -207,16 +185,15 @@ public final class MainForm extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTable2);
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 90, 540, 500));
+        jPanel1.add(jtf_search, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 20, 350, -1));
 
-        jtf_search.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                jtf_searchKeyPressed(evt);
-            }
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                jtf_searchKeyReleased(evt);
+        jb_ok.setText("Ok !");
+        jb_ok.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jb_okActionPerformed(evt);
             }
         });
-        jPanel1.add(jtf_search, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 30, 350, -1));
+        jPanel1.add(jb_ok, new org.netbeans.lib.awtextra.AbsoluteConstraints(1190, 17, 60, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1290, -1));
 
@@ -267,33 +244,10 @@ public final class MainForm extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    private void jc_dayPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jc_dayPropertyChange
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jc_dayPropertyChange
-
-    private void jc_monthPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jc_monthPropertyChange
-        // TODO add your handling code here:
-
-    }//GEN-LAST:event_jc_monthPropertyChange
-
-    private void jc_yearPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jc_yearPropertyChange
-        // TODO add your handling code here:
-    
-    }//GEN-LAST:event_jc_yearPropertyChange
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        preencher_jtable(jc_day.getDay(), jc_month.getMonth()+1, jc_year.getYear());
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jtf_searchKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtf_searchKeyPressed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jtf_searchKeyPressed
-
-    private void jtf_searchKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtf_searchKeyReleased
+    private void jb_okActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_okActionPerformed
         // TODO add your handling code here:
         search_jtable2(Integer.parseInt(jtf_search.getText()));
-    }//GEN-LAST:event_jtf_searchKeyReleased
+    }//GEN-LAST:event_jb_okActionPerformed
 
     /**
      * @param args the command line arguments
@@ -330,7 +284,6 @@ public final class MainForm extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
@@ -348,6 +301,8 @@ public final class MainForm extends javax.swing.JFrame {
     private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
+    private javax.swing.JButton jb_consul;
+    private javax.swing.JButton jb_ok;
     private com.toedter.calendar.JDayChooser jc_day;
     private com.toedter.calendar.JMonthChooser jc_month;
     private com.toedter.calendar.JYearChooser jc_year;
